470523dcae4f92fa9fa4d788997cff08190d30a0
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-04-02 15:15:32 -0600

    Implementacion de React Native Paper

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Button, Card } from 'react-native-paper';
 
 export default function App() {
-  console.log("Hola como estas");
-  return(
-    <SafeAreaView>
-      <Text>Hola Mundo</Text>
-    </SafeAreaView>
+  console.log("Hola como estas?");
+  return (
+    <PaperProvider>
+      <SafeAreaView>
+        <Text>Hola Mundo</Text>
+        <Button mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+
+        <Card>
+          <Card.Actions>
+            <Button>Cancel</Button>
+            <Button>Ok</Button>
+          </Card.Actions>
+        </Card>
+      </SafeAreaView>
+    </PaperProvider>
+
   );
 }
