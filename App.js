d744a151589e6046b66c1f7ea0ac2fc4d1d47b9e
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-04-02 18:30:08 -0600

    Instalacion React Navigation 6

--- App.js
+++ App.js
@@ -2,24 +2,28 @@ import React from 'react'
 import { SafeAreaView, Text } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Button, Card } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   console.log("Hola como estas?");
   return (
     <PaperProvider>
-      <SafeAreaView>
-        <Text>Hola Mundo</Text>
-        <Button icon="image" mode="contained" onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
+      <NavigationContainer>
+        <SafeAreaView>
+          <Text>Hola Mundo</Text>
+          <Button icon="image" mode="contained" onPress={() => console.log('Pressed')}>
+            Press me
+          </Button>
 
-        <Card>
-          <Card.Actions>
-            <Button>Cancel</Button>
-            <Button>Ok</Button>
-          </Card.Actions>
-        </Card>
-      </SafeAreaView>
+          <Card>
+            <Card.Actions>
+              <Button>Cancel</Button>
+              <Button>Ok</Button>
+            </Card.Actions>
+          </Card>
+        </SafeAreaView>
+      </NavigationContainer>
+
     </PaperProvider>
 
   );
