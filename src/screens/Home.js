5db2aa24d934d46aa52f9d8af1f151ccd59077be
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-04-03 18:45:14 -0500

    Obtención de la informacion de las peliculas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { View, Text } from "react-native";
+import { getNewsMoviesApi } from "../api/movies";
 
 export default function Home() {
+    const [newMovies, setNewMovies] = useState(null);
+    
+    console.log(newMovies);
+
+    useEffect(() => {
+        getNewsMoviesApi().then((response) => {
+            setNewMovies(response.results)
+        });
+    }, []);
+    
     return(
         <View>
             <Text>Estamos en Home</Text>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const API_KEY = "d6fa7deb22b862d457b56ee643c9a91b";
+export const API_HOST = 'https://api.themoviedb.org/3';
+export const API_KEY = 'd6fa7deb22b862d457b56ee643c9a91b';
+export const LANG = 'es-ES';
