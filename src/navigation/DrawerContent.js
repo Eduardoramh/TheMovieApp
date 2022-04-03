fe2d5e1d94c08e93fe516b4da6c350b238dd5c7f
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-04-02 22:09:53 -0600

    Personalizacion del contenido del drawer

--- src/navigation/DrawerContent.js
+++ src/navigation/DrawerContent.js
@@ -1,21 +1,23 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { DrawerContentScrollView } from '@react-navigation/drawer';
 import { Drawer, Switch, TouchableRipple, Text } from 'react-native-paper';
 
 export default function DrawerContent(props) {
     const { navigation } = props;
-    const [active, setActive] = useState ("home");
-    console.log(props);
+    const [active, setActive] = useState("home");
+
+    const onChangeScreen = (screen) => {
+        setActive(screen);
+        navigation.navigate(screen);
+    };
 
     return (
         <DrawerContentScrollView style={styles.text}>
             <Drawer.Section>
-                <Drawer.Item
-
-                    label='Inicio'
-                    onPress={() => navigation.navigate("news")}
-                />
+                <Drawer.Item label='Inicio' active={active === "home"} onPress={() => onChangeScreen('home')} />
+                <Drawer.Item label='Peliculas populares' active={active === "popular"} onPress={() => onChangeScreen('popular')}  />
+                <Drawer.Item label='Nuevas Peliculas' active={active === "news"} onPress={() => onChangeScreen('news')}  />
             </Drawer.Section>
         </DrawerContentScrollView>
     );
