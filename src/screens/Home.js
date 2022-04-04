26525165a9dd6ce190f1dc09af8649b4d77ebddc
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-04-04 10:01:18 -0500

    Obteniendo los generos de las peliculas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,38 +1,58 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { Title } from "react-native-paper";
-import { getNewsMoviesApi } from "../api/movies";
+import { getNewsMoviesApi, getAllGenresApi } from "../api/movies";
 import CarouselVertical from "../components/CarouselVertical";
 
 export default function Home(props) {
-    const {navigation} = props;
+    const { navigation } = props;
     const [newMovies, setNewMovies] = useState(null);
-    
+    const [genreMovies, setGenreMovies] = useState(null);
+
 
     useEffect(() => {
         getNewsMoviesApi().then((response) => {
             setNewMovies(response.results)
         });
     }, []);
-    
-    return(
+
+    useEffect(() => {
+        getAllGenresApi().then((response) => {
+            setGenreMovies(response)
+        });
+    }, []);
+
+    return (
         <ScrollView showsVerticalScrollIndicator={false}>
             {newMovies && (
                 <View style={styles.news}>
                     <Title style={styles.newsTitle}>Nuevas películas</Title>
-                    <CarouselVertical data={newMovies} navigation={navigation}/>
+                    <CarouselVertical data={newMovies} navigation={navigation} />
                 </View>
             )}
+
+            <View style={styles.genres}>
+                <Title style={styles.genresTitle}>Películas por genero</Title>
+            </View>
         </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-    news:{
+    news: {
         marginVertical: 10,
     },
-    newsTitle:{
+    newsTitle: {
         marginBottom: 15,
+        marginHorizontal: 20,
+        fontWeight: "bold",
+        fontSize: 22,
+    },
+    genres: {
+        marginTop: 20,
+        marginBottom: 50,
+    },
+    genresTitle: {
         marginHorizontal: 20,
         fontWeight: "bold",
         fontSize: 22,
