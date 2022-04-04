d184dbc948f7066a3c36ea0113e7f31cd211cbb9
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-04-04 15:06:13 -0500

    Obteniendo los datos de la pelicula por ID

--- src/screens/Movie.js
+++ src/screens/Movie.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { View, Text } from "react-native";
+import { getMovieByIdApi } from "../api/movies";
 
 export default function Movie(props) {
-    // console.log(props);
+    const {route} = props;
+    const {id} = route.params;
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+      getMovieByIdApi(id).then((response) => {
+        setMovie(response);
+      })
+    },[])
+    
+
     return(
         <View>
             <Text>Estamos en Movie</Text>
