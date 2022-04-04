c50a207c43c8cfeb82330ac6333ac7468550a855
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-04-03 20:27:30 -0500

    Obtencion de generos de las peliculas

--- src/components/CarouselVertical.js
+++ src/components/CarouselVertical.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Image, Dimensions, TouchableWithoutFeedback } from "react-native"
 import { Text, Title } from "react-native-paper"
 import Carousel from "react-native-snap-carousel"
 import { BASE_PATH_IMG } from "../utils/constants";
+import { getGenreMovieApi } from "../api/movies";
+import { map, size } from "lodash";
 
 const { width } = Dimensions.get('window');
 const ITEM_WIDTH = Math.round(width * 0.7);
@@ -23,16 +25,31 @@ export default function CarouselVertical(props) {
 
 function RenderItem(props) {
     const { data } = props;
-    const { title, poster_path } = data.item;
+    const { title, poster_path, genre_ids } = data.item;
     const imageUrl = `${BASE_PATH_IMG}/w500${poster_path}`
+    const [genres, setGenres] = useState(null);
 
-    console.log(imageUrl);
+    useEffect(() => {
+        getGenreMovieApi(genre_ids).then((response) => {
+            setGenres(response);
+        })
+    }, [])
+
 
     return (
         <TouchableWithoutFeedback onPress={() => console.log('Hola')}>
             <View style={styles.card}>
                 <Image style={styles.image} source={{ uri: imageUrl }} />
                 <Title style={styles.title}>{title}</Title>
+                <View style={styles.genres}>
+                    {genres &&
+                        map(genres, (genre, index) => (
+                            <Text key={index} style={styles.genre}>
+                                {genre}
+                                {index !== size(genres) -1 && ',  '} 
+                            </Text>
+                        ))}
+                </View>
             </View>
         </TouchableWithoutFeedback>
     )
@@ -56,5 +73,13 @@ const styles = StyleSheet.create({
     title: {
         marginHorizontal: 10,
         marginTop: 10,
+    },
+    genres: {
+        flexDirection: "row",
+        marginHorizontal: 10,
+    },
+    genre: {
+        fontSize: 12,
+        color: "#8997a5",
     }
 });
