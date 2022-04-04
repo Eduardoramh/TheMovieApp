8e99c39b611ee40e70d2701afb14bc0ce386d8fb
Eduardoramh <lalo.ramirezh102@gmail.com>
2022-04-03 19:55:47 -0500

    Implementacion de imagenes y estilos al carousel

--- src/components/CarouselVertical.js
+++ src/components/CarouselVertical.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { StyleSheet, View, Image, Dimensions, TouchableWithoutFeedback, Text } from "react-native"
+import { StyleSheet, View, Image, Dimensions, TouchableWithoutFeedback } from "react-native"
+import { Text, Title } from "react-native-paper"
 import Carousel from "react-native-snap-carousel"
+import { BASE_PATH_IMG } from "../utils/constants";
 
-const {width} = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 const ITEM_WIDTH = Math.round(width * 0.7);
 
 export default function CarouselVertical(props) {
-    const {data} = props;
+    const { data } = props;
 
     return (
         <Carousel
@@ -19,12 +21,40 @@ export default function CarouselVertical(props) {
     )
 }
 
-function RenderItem(props){
-    const {} = props;
+function RenderItem(props) {
+    const { data } = props;
+    const { title, poster_path } = data.item;
+    const imageUrl = `${BASE_PATH_IMG}/w500${poster_path}`
 
-    return(
-        <View>
-            <Text>Pelicula</Text>
-        </View>
+    console.log(imageUrl);
+
+    return (
+        <TouchableWithoutFeedback onPress={() => console.log('Hola')}>
+            <View style={styles.card}>
+                <Image style={styles.image} source={{ uri: imageUrl }} />
+                <Title style={styles.title}>{title}</Title>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
+
+const styles = StyleSheet.create({
+    card: {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+    },
+    image: {
+        width: "100%",
+        height: 450,
+        borderRadius: 20,
+    },
+    title: {
+        marginHorizontal: 10,
+        marginTop: 10,
+    }
+});
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const API_HOST = 'https://api.themoviedb.org/3';
 export const API_KEY = 'd6fa7deb22b862d457b56ee643c9a91b';
 export const LANG = 'es-ES';
+export const BASE_PATH_IMG = 'https://image.tmdb.org/t/p';
